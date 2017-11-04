4f809b33927fcd071597f17a6da150c48d3c3406
Alain Chautard <alain@interstate21.com>
2017-11-04 12:01:58 -0700

    Added Material Toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { UserEntryComponent } from './user-entry/user-entry.component';
 import { EntryService } from './entry.service';
 import { EntriesComponent } from './entries/entries.component';
-import {MatButtonModule, MatChipsModule, MatInputModule} from "@angular/material";
+import {MatButtonModule, MatChipsModule, MatInputModule, MatToolbarModule} from "@angular/material";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
@@ -19,6 +19,7 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     MatInputModule,
     MatButtonModule,
     MatChipsModule,
+    MatToolbarModule,
     BrowserAnimationsModule
   ],
   providers: [EntryService],
