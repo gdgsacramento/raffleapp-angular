13d6389c5b5db88e5cc05cab64cf70f529b4484e
Alain Chautard <alain@interstate21.com>
2018-11-11 17:21:44 -0800

    Updated prod environment for CI build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyACzmGxAtEhVTCTLNwFEKtmoGheXc43-k8",
+    authDomain: "raffle-gdgsac.firebaseapp.com",
+    databaseURL: "https://raffle-gdgsac.firebaseio.com",
+    projectId: "firebase-raffle-gdgsac",
+    storageBucket: "firebase-raffle-gdgsac.appspot.com",
+    messagingSenderId: "1033592383172"
+  }
 };
