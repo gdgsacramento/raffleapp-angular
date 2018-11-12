86b8c26714428f71fde61fe1593895ade5610554
Alain Chautard <alain@interstate21.com>
2018-11-11 17:34:21 -0800

    Running prod build instead of tests

--- karma.conf.js
+++ karma.conf.js
@@ -19,13 +19,23 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
-    
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless','--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222']
+      }
+    },
+
+
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['ChromeHeadless'],
+    singleRun: true
   });
 };
